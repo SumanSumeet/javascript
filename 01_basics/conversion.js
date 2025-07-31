04fbb8c2b2fb14ea0a82e4134e0c724c1a241a95
SumanSumeet <sumansumeet.iiitu@gmail.com>
2025-07-31 17:46:58 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+
+//Notes
+//"33" => 33
+//"33abc" => NaN
+// true -> 1; false => 0
+
+let isloggedin = 1
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
