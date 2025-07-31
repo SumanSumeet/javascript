31bba86407901d6a4e7c4545a7fbca779f8d98b9
SumanSumeet <sumansumeet.iiitu@gmail.com>
2025-07-31 17:32:12 +0000

    datatypes and variables

---
+++ 01_basics/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3+3);
+
+console.log("suman");
+
+let name = "suman"
+let age = 19
+
+//some imp datatypes are
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol
+
+console.log(typeof "suman");
+console.log(typeof null);
+
+
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountid= "14354"
+let accountemail="suman123@gmail.com"
+var accountpassword = "34233"
+accountcity="una"
+
+console.log(accountid);
+
+console.table([accountid, accountemail, accountpassword, accountcity]);
+
