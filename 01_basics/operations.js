7cdcae5b3e98e1932b31cc8a97d3710162db7331
SumanSumeet <sumansumeet.iiitu@gmail.com>
2025-07-31 17:59:33 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,27 @@
+//some basic operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="suman"
+let str2 ="sumeet"
+
+let str3 = str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+3);
+console.log(1+2+"3");
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
+
+
+
+
+
