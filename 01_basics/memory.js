713b215c717f351bd33afdad63bfe746cb6793bb
SumanSumeet <sumansumeet.iiitu@gmail.com>
2025-08-03 06:13:22 +0000

    memory

---
+++ 01_basics/memory.js
@@ -0,0 +1,21 @@
+// Stack (primitive) , Heap(Non primitive)
+
+const { useInsertionEffect } = require("react");
+
+let myyoutubename = sumansumeet
+
+let anothername = myyoutubename
+anothername = "aaashishranjan"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let user = {
+     email:"user@google.com" , 
+     upi: "user@ybl"
+}
+
+let usertwo = user
+
+usertwo.email = "suman@gmail.com"
+
